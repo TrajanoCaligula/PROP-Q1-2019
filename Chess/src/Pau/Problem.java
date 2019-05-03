64cb1caa8d8f7bf8fb1be39bcac29553c84b67b4
pau.charques <pau.charques@est.fib.upc.edu>
2019-05-03 11:04:16 +0200

    Docu

--- Chess/src/Pau/Problem.java
+++ Chess/src/Pau/Problem.java
@@ -361,6 +361,7 @@ public class Problem{
         if(splits.length != 6){
             System.out.println("The FEN introduced it's not in the correct format. EXAMPLE: ");
             System.out.println("[FEN=\"1N1b4/6nr/R5n1/2Ppk2r/K2p2qR/8/2N1PQ2/B6B w - - 0 1\"]");
+            System.exit(0);
         }
 
         String vFEN = splits[0].substring(6);
